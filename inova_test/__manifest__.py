abbb65259b73c8ad6328fa4afa0c45334c17a396
Abdurrahman Saber <abdelrahmansaber85@gmail.com>
2024-06-01 16:13:57 +0300

    [imp] inova_test: add security csv to manifest

--- inova_test/__manifest__.py
+++ inova_test/__manifest__.py
@@ -8,6 +8,7 @@
     'version': '14.0.1',
     'depends': ['base', 'point_of_sale', 'sale', 'product'],
     'data': [
+        'security/ir.model.access.csv',
         'views/pos_config_views.xml',
         'views/product_template_views.xml',
         'views/pos_assets.xml',
